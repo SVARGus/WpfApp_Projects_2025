713e0f585b3faad955f881d1bd2b7e4d5273fe0a
SVARGus <kyznetsovpn@mail.ru>
2025-02-27 23:57:42 +0300

    Именование полей, создание событие и частичное написание функционала

--- Wpf_Calculator/CalculatorWindow.xaml.cs
+++ Wpf_Calculator/CalculatorWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace Wpf_Calculator
 {
@@ -22,6 +24,112 @@ namespace Wpf_Calculator
         public CalculatorWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonFloat_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonEqual_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonPlus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonMinus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonMultiplication_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonDivide_Click(object sender, RoutedEventArgs e)
+        {
+            if(Regex.IsMatch(mathExpression.Text, @"[\+\-\*/]"))
+            {
+                string result = mathExpression.Text.Remove(mathExpression.Text.Length - 1) + "/";
+                mathExpression.Text = result;
+            }
+        }
+
+        private void ButtonBackspace_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonClear_Click(object sender, RoutedEventArgs e)
+        {
+            mathExpression.Text = string.Empty;
+            result.Text = "0";
+        }
+
+        private void ButtonClearEntry_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Result() // Вычисление суммы
+        {
+            DataTable dt = new DataTable();
+            var res = dt.Compute(mathExpression.Text, "");
+            result.Text = res.ToString();
         }
     }
 }
