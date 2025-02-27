82037193bc43f580f85f82bc1ef34cdede84b851
SVARGus <kyznetsovpn@mail.ru>
2025-02-27 13:49:55 +0300

    правки после переименования окна (класса)

--- Wpf_Calculator/MainWindow.xaml.cs
+++ Wpf_Calculator/MainWindow.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Wpf_Calculator
 {
     /// <summary>
-    /// Логика взаимодействия для CalculatorWindow.xaml
+    /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class CalculatorWindow : Window
+    public partial class MainWindow : Window
     {
-        public CalculatorWindow()
+        public MainWindow()
         {
             InitializeComponent();
         }
