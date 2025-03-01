9da254b680595ee961e8230e2ed1f18ab3e0cd78
SVARGus <kyznetsovpn@mail.ru>
2025-03-01 21:14:39 +0300

    Выполненная в классе часть работы

---
+++ Wpf_HW_Control-Brushes-fonts_28_02_2025/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Wpf_HW_Control_Brushes_fonts_28_02_2025
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
