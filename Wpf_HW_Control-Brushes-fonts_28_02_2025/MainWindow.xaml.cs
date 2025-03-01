1755703966f96a769854b2de8c99700296c1bdea
SVARGus <kyznetsovpn@mail.ru>
2025-03-01 23:10:19 +0300

    Доработка наполнения (полная реализация). Создание событий на слайдеры Цвета фона и обработка. Блок цвет фона реализован не полностью

--- Wpf_HW_Control-Brushes-fonts_28_02_2025/MainWindow.xaml.cs
+++ Wpf_HW_Control-Brushes-fonts_28_02_2025/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,46 @@ namespace Wpf_HW_Control_Brushes_fonts_28_02_2025
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ARGB_Slider_ValueChanged_Background(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Slider select = sender as Slider;
+            if(select != null)
+            {
+                if (select.Name == "ARGBaSlider" && ARGBaValue != null)
+                {
+                    ARGBaValue.Text = select.Value.ToString();
+                }
+                else if(select.Name == "ARGBrSlider" && ARGBrValue != null)
+                {
+                    ARGBrValue.Text = select.Value.ToString();
+                }
+                else if (select.Name == "ARGBgSlider" && ARGBgValue != null)
+                {
+                    ARGBgValue.Text = select.Value.ToString();
+                }
+                else if (select.Name == "ARGBbSlider" && ARGBbValue != null)
+                {
+                    ARGBbValue.Text = select.Value.ToString();
+                }
+            }
+            UpdateBackgroundColor();
+        }
+        private void UpdateBackgroundColor() // Обновление цвета фона после изменений
+        {
+            byte a = (byte)ARGBaSlider.Value;
+            byte r = (byte)ARGBrSlider.Value;
+            byte g = (byte)ARGBgSlider.Value;
+            byte b = (byte)ARGBbSlider.Value;
+            Color backgroundColor = Color.FromArgb(a, r, g, b);
+            testText.Background = new SolidColorBrush(backgroundColor);
+            selectARGB.Text = testText.Background.ToString();
         }
     }
 }
