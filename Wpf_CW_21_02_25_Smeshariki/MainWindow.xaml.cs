49d57268fefeb23d2470d1912dbf52da207c0cb3
SVARGus <kyznetsovpn@mail.ru>
2025-03-01 11:33:54 +0300

    Завершено классная работа и реализована логика

--- Wpf_CW_21_02_25_Smeshariki/MainWindow.xaml.cs
+++ Wpf_CW_21_02_25_Smeshariki/MainWindow.xaml.cs
@@ -20,9 +20,89 @@ namespace Class_work_p2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<int> resultTest = new List<int>() { 0, 0, 0, 0, 0 };
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            RadioButton select = (RadioButton)sender;
+            if(select.Content.ToString() == "Лосяш")
+            {
+                resultTest[0] = 1;
+            }
+            else
+            {
+                resultTest[0] = 0;
+            }
+        }
+
+        private void RadioButton_Checked_2(object sender, RoutedEventArgs e)
+        {
+            RadioButton select = (RadioButton)sender;
+            if (select.Content.ToString() == "Крош")
+            {
+                resultTest[1] = 1;
+            }
+            else
+            {
+                resultTest[1] = 0;
+            }
+        }
+
+        private void RadioButton_Checked_3(object sender, RoutedEventArgs e)
+        {
+            RadioButton select = (RadioButton)sender;
+            if (select.Content.ToString() == "Копатыч")
+            {
+                resultTest[2] = 1;
+            }
+            else
+            {
+                resultTest[2] = 0;
+            }
+        }
+
+        private void RadioButton_Checked_4(object sender, RoutedEventArgs e)
+        {
+            RadioButton select = (RadioButton)sender;
+            if (select.Content.ToString() == "Нюша")
+            {
+                resultTest[3] = 1;
+            }
+            else
+            {
+                resultTest[3] = 0;
+            }
+        }
+        private void RadioButton_Checked_5(object sender, RoutedEventArgs e)
+        {
+            RadioButton select = (RadioButton)sender;
+            if (select.Content.ToString() == "Совунья")
+            {
+                resultTest[4] = 1;
+            }
+            else
+            {
+                resultTest[4] = 0;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int result = resultTest.Sum();
+            string messageTest;
+            if (result > 0 && result < 3)
+                messageTest = "Новичек";
+            else if (result > 2 && result < 5)
+                messageTest = "Знаток";
+            else if (result == 5)
+                messageTest = "Эксперт";
+            else
+                messageTest = "Error";
+            MessageBox.Show(messageTest);
         }
     }
 }
