f4596db670932e2c4a86704f2ad5d6f0057739f8
SVARGus <kyznetsovpn@mail.ru>
2025-03-17 17:56:05 +0300

    Предварительный проект структуры проекта, выделение иконок в ресурсы, создание классов погоды

---
+++ Wpf_WeatherPanel/Models/HourlyForecastModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_WeatherPanel.Models
+{
+    public class HourlyForecastModel
+    {
+        public DateTime Time { get; set; }
+        public float Temperature { get; set; }
+        public float ApparentTemperature { get; set; }
+        public float RelativeHumidity { get; set; }
+        public float SurfasePressure { get; set; }
+        public float WindSpeed { get; set; }
+        public int WindDirection { get; set; }
+        public WeatherCodes Weather { get; set; } // что за WeatherCodes???
+    }
+}
---
+++ Wpf_WeatherPanel/Models/WeatherDay.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_WeatherPanel.Models
+{
+    public class WeatherDay
+    {
+        public DateTime Date { get; set; } // Текущая дата
+        public string WeekDay { set; get; } // День недели
+        public string Location { set; get; } // Название локации
+        public double MaxTemperature { set; get; } // Максимальная температура
+        public double MinTemperature { set; get; } // минимальная температура
+        public double DayTemperature { set; get; } // Дневная температура
+        public double NightTemperature { set; get; } // ночная температура
+        public string ConditionWeather { set; get; } // Обозначение погоды (облачно, осадки, солнечно), можно по идее вынести в Enum
+        public double WindSpeed { set; get; } // Скорость ветра
+        public double WindGust { set; get; } // Порывы ветра
+        public int WindDirection { set; get; } // направление ветра (градусы)
+        public string WindCardinal { set; get; } // Направление ветра (WE, SW, W и т.д.), можно сделать через enum
+        public double Humidity { set; get; } // Влажность в %
+        public double Pressure { set; get; } // Атмосферное давление
+        public List<HourlyForecastModel> DayWeather {set; get;} // Лист почасовой разбивки погоды
+    }
+}
