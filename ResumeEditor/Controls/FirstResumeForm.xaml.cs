353181f2efb8eeaf7f66bc97f8771fbda6ded884
SVARGus <kyznetsovpn@mail.ru>
2025-03-21 17:54:15 +0300

    Создано главное окно с разбивкой, создана модель данных, подобраны изображения иконок, частично создана первая форма

---
+++ ResumeEditor/Controls/FirstResumeForm.xaml.cs
@@ -0,0 +1,58 @@
+﻿using ResumeEditor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ResumeEditor.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для FirstResumeForm.xaml
+    /// </summary>
+    public partial class FirstResumeForm : UserControl
+    {
+        public UserWorker userWorker = new UserWorker() // тестовые данные
+        {
+            DesiredPosition = "Software Developer",
+            FirstName = "Иван",
+            SecondName = "Иванов",
+            Age = "34",
+            BirthDay = new DateTime(1990, 5, 15),
+            CityResidence = "Москва",
+            Photo = "/Resources/Image/Тестовое фото.png",
+            Phone = "+7 (999) 123-45-67",
+            Email = "ivan.ivanov@example.com",
+            PositionWork = "Junior Developer",
+            CompanyWork = "ООО Ромашка",
+            GettingStartWork = new DateTime(2018, 6, 1),
+            EndWork = new DateTime(2023, 5, 31),
+            DescriptionLastWork = "Разработка и поддержка веб-приложений.",
+            BasicEducation = "Высшее",
+            EducationInstitution = "Московский государственный университет",
+            Faculty = "Факультет вычислительной математики и кибернетики",
+            Specialitu = "Прикладная математика и информатика",
+            EndEducation = new DateTime(2017, 6, 30),
+            AboutMe = "Увлекаюсь программированием, изучаю новые технологии и frameworks."
+        };
+        public FirstResumeForm()
+        {
+            this.DataContext = userWorker;
+            InitializeComponent();
+            if (DataContext == null)
+            {
+                throw new Exception("DataContext не установлен.");
+            }
+        }
+    }
+}
---
+++ ResumeEditor/Models/UserWorker.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResumeEditor.Models
+{
+    public class UserWorker
+    {
+        public UserWorker()
+        {
+        }
+
+        public UserWorker(string desiredPosition, string firstName, string secondName, string age, DateTime birthDay, string cityResidence, string photo, 
+            string phone, string email, 
+            string positionWork, string companyWork, DateTime gettingStartWork, DateTime endWork, string descriptionLastWork, 
+            string basicEducation, string educationInstitution, string faculty, string specialitu, DateTime endEducation, 
+            string aboutMe)
+        {
+            DesiredPosition = desiredPosition;
+            FirstName = firstName;
+            SecondName = secondName;
+            Age = Age;
+            BirthDay = birthDay;
+            CityResidence = cityResidence;
+            Photo = photo;
+
+            Phone = phone;
+            Email = email;
+
+            PositionWork = positionWork;
+            CompanyWork = companyWork;
+            GettingStartWork = gettingStartWork;
+            EndWork = endWork;
+            DescriptionLastWork = descriptionLastWork;
+
+            BasicEducation = basicEducation;
+            EducationInstitution = educationInstitution;
+            Faculty = faculty;
+            Specialitu = specialitu;
+            EndEducation = endEducation;
+
+            AboutMe = aboutMe;
+        }
+
+        // Личные данные
+        public string DesiredPosition { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Age { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string CityResidence { get; set; }
+        public string Photo { get; set; }
+        
+        // Контакты
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        // Последний опыт работы (позже вывести в отдельный класс, а сдесь хранить лист прошлых мест работы)
+        public string PositionWork { get; set; }
+        public string CompanyWork { get; set; }
+        public DateTime GettingStartWork { get; set; }
+        public DateTime EndWork { get; set; }
+        public string DescriptionLastWork { get; set; }
+        
+        // Оброзование (позже вывести в отдельный класс, а сдесь хранить лист оброзовательных учреждений)
+        public string BasicEducation { get; set; }
+        public string EducationInstitution { get; set; }
+        public string Faculty { get; set; }
+        public string Specialitu { get; set; }
+        public DateTime EndEducation { get; set; }
+
+        // Дополнительные поля
+        public string AboutMe { get; set; }
+
+
+    }
+}
